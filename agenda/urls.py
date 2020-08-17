3a4d097348c1730bc5543e9fa52bc4990e1624db
lmbernardo7520112 <lmbernardo752011@gmail.com>
2020-08-17 16:21:29 -0300

    Incluido template para inserção de eventos da agenda

--- agenda/urls.py
+++ agenda/urls.py
@@ -21,6 +21,11 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    #path('', RedirectView.as_view.(url='/agenda/'))
-    path('', views.index)
+    path('agenda/evento/submit', views.submit_evento),
+    #path('', RedirectView.as_view.(url='/agenda/')),
+    path('', views.index),
+    path('agenda/evento/', views.evento),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
